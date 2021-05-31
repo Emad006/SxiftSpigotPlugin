ea96f0fc518c7f1b6def907735d486d89ff0e6cd
XyroPhyte <emadharun.eh@gmail.com>
2021-05-31 16:14:13 +0600

    Added commands and minor update

--- src/main/java/events/OnDeath.java
+++ src/main/java/events/OnDeath.java
@@ -1,6 +1,5 @@
 package events;
 
-import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -10,7 +9,5 @@ public class OnDeath implements Listener {
     @EventHandler
     public void onDeath(PlayerDeathEvent event) {
         event.getEntity().sendMessage("You died.");
-        event.getEntity().sendMessage("Changing GameMode to Spectator.");
-        event.getEntity().setGameMode(GameMode.SPECTATOR);
     }
 }
--- src/main/java/me/xyrophyte/sxift/Sxift.java
+++ src/main/java/me/xyrophyte/sxift/Sxift.java
@@ -1,6 +1,10 @@
 package me.xyrophyte.sxift;
 
 import events.*;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Sxift extends JavaPlugin {
@@ -23,5 +27,18 @@ public final class Sxift extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
         getLogger().info("Plugin is shutting down.");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equals("suicide")) {
+            if (sender instanceof Player) {
+                ((Player) sender).setHealth(0);
+                sender.sendMessage(ChatColor.BLUE + ((Player) sender).getDisplayName() + ChatColor.RED + " gave up on life.");
+            } else {
+                getLogger().warning(ChatColor.DARK_RED + " Failed to run command. " + ChatColor.YELLOW + "Reason: " + ChatColor.RED + "Not a Player.");
+            }
+        }
+        return false;
     }
 }
