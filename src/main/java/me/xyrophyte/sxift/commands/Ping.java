56234ca18c099a3b92f4aac421d4ed9e1615f329
XyroPhyte <emadharun.eh@gmail.com>
2021-06-04 04:44:58 +0600

    DoubleCheck taught me how to use constructors!

--- src/main/java/me/xyrophyte/sxift/commands/Ping.java
+++ src/main/java/me/xyrophyte/sxift/commands/Ping.java
@@ -5,6 +5,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class Ping implements CommandExecutor {
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         sender.sendMessage("Pong!");
--- src/main/java/me/xyrophyte/sxift/events/OnEnterBed.java
+++ src/main/java/me/xyrophyte/sxift/events/OnEnterBed.java
@@ -7,7 +7,6 @@ import org.bukkit.event.player.PlayerBedEnterEvent;
 public class OnEnterBed implements Listener {
 
     @EventHandler
-
     public void onEnterBed(PlayerBedEnterEvent event) {
         event.getPlayer().sendMessage("Good Night!");
     }
